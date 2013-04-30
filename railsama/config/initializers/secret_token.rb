3b452dbe8605e21e67204b2f8705dbc6ac83d60d
elanakoren <elanakoren@gmail.com>
2013-04-30 15:20:51 -0400

    Revert "Revert "Revert "Hooked up to RoR"""
    This reverts commit c00d3b10ec0a707bc046c0be31489110e15cff49.

--- railsama/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Railsama::Application.config.secret_token = 'f62732738eabe91a2604fdf52ab5c60a72184030e92078e14f0088e916c4e9256ee3421dcb1383d782e6846310468ac08c2b8c144a26e9e903dbbcb0e00c9f60'
