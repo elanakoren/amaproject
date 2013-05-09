92a435ccfda1d22d128f17d9a80aef737c8534b3
elanakoren <elanakoren@gmail.com>
2013-05-08 21:40:26 -0400

    now saves questions and answers in the list

--- railsama/railsama/app/controllers/comments_controller.rb
+++ railsama/railsama/app/controllers/comments_controller.rb
@@ -22,8 +22,10 @@ class CommentsController < ApplicationController
 	@resp_text = @resp.body
 	@result = JSON.parse(@resp_text)
 	@op = @result[0]['data']['children'][0]["data"]["author"]
-	@list = Array.new 
-	toplevel(@result, @list, @op) 
+	@list = Array.new
+	@masterlist = Array.new 
+	toplevel(@result, @list, @op)
+	find_parent(@list, @result, @masterlist) 
 end
 
    def toplevel(result, list, op)    
@@ -37,17 +39,42 @@ def traverse_thread(thread, list, op)
 		h = {:body => thread['body'], :unique_id => thread['name'], :parent_id => thread['parent_id'], :author => thread['author']}
             	comm = Comment.new(h)  
             	@list.push(comm)
-
         end
         unless thread['replies'].nil? or thread['replies']['data'].nil?
             thread['replies']['data']['children'].each do |reply|
                 traverse_thread(reply['data'], @list, op)
         end
     end
-	return @list
 end
 
-	
+def find_parent(list, result, masterlist)
+    list.each do |comment|
+        top_parent(comment, result, masterlist)
+    end
+end
+
+def top_parent(comment, result, masterlist)
+    result[1]['data']['children'].each do |x|
+        get_parent(x['data'], comment, masterlist)
+    end
+end
+
+def get_parent(thread, comment, masterlist)
+    if thread['name'] == comment.parent_id
+        parent = Comment.new
+        parent.body = thread['body']
+        parent.unique_id = thread['name']
+        parent.parent_id = thread['parent_id']
+        parent.author = thread['author']
+        sublist = [parent, comment]
+        masterlist.concat(sublist)
+    end
+    unless thread['replies'].nil? or thread['replies']['data'].nil?
+        thread['replies']['data']['children'].each do |reply|
+            get_parent(reply['data'], comment, masterlist)
+        end
+    end
+end	
 
   # GET /comments/1
   # GET /comments/1.json
